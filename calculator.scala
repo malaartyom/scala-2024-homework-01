e4d36df04d79454e454fff757bbe698afb46f2f7
malaartyom <malarenkoart@bk.ru>
2024-09-13 23:15:38 +0700

    add task

--- calculator.scala
+++ calculator.scala
@@ -12,13 +12,31 @@
     */
   def parseInt(s: String): Int = s.toInt
 
-  /** Representation of `acc` register. */
-  var acc: Int = 0
-  // define additional registers here
-
-  for (c <- commands) {
-    // implement your calculator's logic here
+  def isNumber(s: String): Boolean = {
+    s.matches("-?\\d+")
   }
 
+
+  /** Representation of `acc` register. */
+  var acc: Int = 0
+  var A: Int = 0
+  var B: Int = 0
+  var blink: Boolean = false
+
+
+  for (c <- commands) {
+      c match {
+        case "+" => acc = A + B; blink = false
+        case "-" => acc = A - B; blink = false
+        case "*" => acc = A * B; blink = false
+        case "/" => if B == 0 then acc = 0; A = 0; B = 0 else acc = A / B; blink = false
+        case "swap" => var temp = A; A = B; B = temp
+        case "blink" => blink = !(blink)
+        case "acc" => if blink then B = acc else A = acc; blink = !(blink)
+        case i if isNumber(i) => if blink then B = parseInt(i) else A = parseInt(i)
+        case x => println(x)
+
+      }
+  }
   println(acc)
 }
