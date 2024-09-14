edc4550726b0afb98ea34d0511dc29d93ec8d82c
malaartyom <malarenkoart@bk.ru>
2024-09-14 14:39:51 +0700

    add task

--- calculator.scala
+++ calculator.scala
@@ -1,42 +1,53 @@
-/** Software implementation of PROC (PROstoy Calculator) mk. 1 (or mk. 2).
-  *
-  * You should finish this procedure according to
-  * the reference described in `README.md` to complete
-  * the assignment.
-  */
-@main def calculator(commands: String*): Unit = {
-  /** Converts given string `s` to integer.
-    *
-    * Throws [[NumberFormatException]] if `s` can't be converted to integer,
-    * but you shouldn't worry about it at this moment.
-    */
-  def parseInt(s: String): Int = s.toInt
+package calc
 
+import scala.util.boundary, boundary.break
+
+def calculator(commands: String*): Int = {
+  def parseInt(s: String): Int = s.toInt
   def isNumber(s: String): Boolean = {
     s.matches("-?\\d+")
   }
 
 
-  /** Representation of `acc` register. */
   var acc: Int = 0
   var A: Int = 0
   var B: Int = 0
   var blink: Boolean = false
 
-
-  for (c <- commands) {
+  boundary:
+    for (c <- commands) {
       c match {
         case "+" => acc = A + B; blink = false
         case "-" => acc = A - B; blink = false
         case "*" => acc = A * B; blink = false
-        case "/" => if B == 0 then acc = 0; A = 0; B = 0 else acc = A / B; blink = false
+        case "/" => if B == 0 then {
+          acc = 0; A = 0
+        } else acc = A / B; blink = false
         case "swap" => var temp = A; A = B; B = temp
         case "blink" => blink = !(blink)
         case "acc" => if blink then B = acc else A = acc; blink = !(blink)
-        case i if isNumber(i) => if blink then B = parseInt(i) else A = parseInt(i)
-        case x => println(x)
+        case i if isNumber(i) => if blink then B = parseInt(i) else A = parseInt(i); blink = !(blink)
+        case "break" => break()
+        case x => ???
 
       }
-  }
-  println(acc)
+    }
+  // println(acc)
+  return acc
+}
+
+@main def main(commands: String*): Unit = {
+  assert(calculator("22", "20", "+") == 42)
+  assert(calculator("22", "20", "-") == 2)
+  assert(calculator("22", "20", "swap", "-") == -2)
+  assert(calculator("22", "20", "+") == 42)
+  assert(calculator("20", "20", "+", "acc", "2", "+") == 42)
+  assert(calculator("20", "20", "+", "acc", "2", "/") == 20)
+  assert(calculator("20", "2", "*") == 40)
+  assert(calculator("20", "0", "/") == 0)
+  assert(calculator("blink", "1", "-") == -1)
+  assert(calculator("break", "1", "2", "+", "3", "4", "+") == 0)
+  assert(calculator("1", "2", "+", "break", "acc", "0", "/") == 3)
+  println("All tests have passed!")
+
 }
